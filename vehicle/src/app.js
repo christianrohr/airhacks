14fa705a14155c9b05cf86b8e894fc0410dde0ba
Adam Bien <abien@adam-bien.com>
2019-10-25 10:33:28 +0200

    simple sample

---
+++ vehicle/src/app.js
@@ -0,0 +1,25 @@
+class Hugo { 
+
+    constructor() { 
+    }
+
+    message() { 
+        return "good morning";
+    }
+
+    get msg() { 
+        return "hello property";
+    }
+
+    set msg(message) { 
+        console.log('hello ' + message);
+    }
+}
+
+const hugo = new Hugo();
+console.log(hugo.message());
+
+const message = hugo.msg;
+hugo.msg = "hello from CLI";
+
+

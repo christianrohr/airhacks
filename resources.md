e1e824863a7dfb86f3608c1bf69cd2ff5f0579af
Adam Bien <abien@adam-bien.com>
2019-10-25 11:29:09 +0200

    querySelector API

--- resources.md
+++ resources.md
@@ -4,3 +4,4 @@ https://blog.mozilla.org/blog/2017/10/18/mozilla-brings-microsoft-google-w3c-sam
 
 https://support.microsoft.com/en-us/help/17454/lifecycle-faq-internet-explorer
 https://devdocs.io
+https://docs.emmet.io/cheat-sheet/
--- vehicle/src/app.js
+++ vehicle/src/app.js
@@ -1,11 +1,6 @@
-const loadModule = async _ => {
-
-    const module = await import('./Modules.js');
-    const tractor = new module.default();
-    console.log(tractor.message(),module.message,module.speed());
+const items = document.querySelectorAll('li');
+items.forEach(it => it.innerText = "new");
 
 
-}
-
-
-loadModule();
+const list = document.querySelector('ol');
+console.log(list);

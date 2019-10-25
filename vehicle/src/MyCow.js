952dc8e308b5d47068cc28c4a511e07a7c44befe
Adam Bien <abien@adam-bien.com>
2019-10-25 13:17:33 +0200

    shadow dom explained

--- vehicle/src/MyCow.js
+++ vehicle/src/MyCow.js
@@ -3,16 +3,21 @@ class MyCow extends HTMLElement {
     constructor() { 
         super();
         console.log('initialized');
+        this.buttonCaption = "Click ME";
+        this.martin = "chief"
+        this.root = this.attachShadow({mode:"open"});
+
     }
 
     connectedCallback() { 
         console.log('inside DOM');
-        this.innerHTML = `
-            <h2>milk</h2>
-            <button>click</button>
+        this.root.innerHTML = `
+            <h2 class=${this.martin}>milk</h2>
+            <button>${this.buttonCaption} ${this.getAttribute('cowname')}</button>
         `;
-        this.button = this.querySelector("button");
+        this.button = this.root.querySelector("button");
         this.button.onclick = _ => this.buttonClicked();
+        console.dir(this);
     }
 
     buttonClicked() { 
@@ -21,7 +26,6 @@ class MyCow extends HTMLElement {
 
     disconnectedCallback() { 
         console.log('cleanup');
-
     }
 
 

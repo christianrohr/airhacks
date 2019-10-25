a6862d0f20196da73102fe3abad23fa85936c03d
Adam Bien <abien@adam-bien.com>
2019-10-25 10:44:19 +0200

    try-catch

--- vehicle/src/app.js
+++ vehicle/src/app.js
@@ -1,5 +1,12 @@
-const serverCall = (uri) => { 
-    fetch('vehicle.json').then(r => r.json()).then(result => console.log(result));
+const serverCall = async (uri) => { 
+    try {
+        const response = await fetch('vehicl.json');
+        const payload = await response.json();
+        const { name, power } = payload;
+    } catch (error) { 
+        console.log('problem with fetch',error);
+    }
+    //console.log(name,power);
     
 }
 

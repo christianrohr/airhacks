74fb0d54c9dce55ae72d0410ac2ccfe5f844c795
Adam Bien <abien@adam-bien.com>
2019-10-25 12:02:37 +0200

    buttonClicked event listener

--- vehicle/src/MyCow.js
+++ vehicle/src/MyCow.js
@@ -1,10 +1,27 @@
 class MyCow extends HTMLElement { 
 
+    constructor() { 
+        super();
+    }
+
     connectedCallback() { 
         this.innerHTML = `
             <h2>milk</h2>
+            <button>click</button>
         `;
+        this.button = this.querySelector("button");
+        this.button.onclick = _ => this.buttonClicked();
     }
+
+    buttonClicked() { 
+        console.log('clicked');
+    }
+
+    disconnectedCallback() { 
+        console.log('cleanup');
+
+    }
+
 
 }
 customElements.define('my-cow',MyCow);

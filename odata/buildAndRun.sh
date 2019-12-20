cb587db2c0efdefce85aa9b3627ffffe599a675d
Rohr Christian <Christian.Rohr@farmfacts.de>
2019-12-20 13:04:29 +0100

    clear master

--- odata/buildAndRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn clean package && docker build -t com.airhacks/odata-hello .
-docker rm -f odata-hello || true && docker run -d -p 8080:8080 -p 4848:4848 --name odata-hello com.airhacks/odata-hello 
